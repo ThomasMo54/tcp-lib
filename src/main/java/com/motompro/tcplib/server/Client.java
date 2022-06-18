2186d8d28872ab53cb03560cafa8ec787c6a558c
TomMo54 <tmoretti@motompro.com>
2022-06-19 00:06:26 +0200

    feat: Created connection thread

--- src/main/java/com/motompro/tcplib/server/Client.java
+++ src/main/java/com/motompro/tcplib/server/Client.java
@@ -1,13 +1,20 @@
 package com.motompro.tcplib.server;
 
 import java.net.Socket;
+import java.util.UUID;
 
 public class Client {
 
+    private final UUID uuid;
     private final Socket socket;
 
-    public Client(Socket socket) {
+    public Client(UUID uuid, Socket socket) {
+        this.uuid = uuid;
         this.socket = socket;
+    }
+
+    public UUID getUuid() {
+        return uuid;
     }
 
     public String getIp() {
--- src/main/java/com/motompro/tcplib/server/Server.java
+++ src/main/java/com/motompro/tcplib/server/Server.java
@@ -2,20 +2,47 @@ package com.motompro.tcplib.server;
 
 import java.io.IOException;
 import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 
 public class Server {
 
     private final ServerSocket serverSocket;
+    private final Map<UUID, Client> clients = new HashMap<>();
 
     public Server() throws IOException {
         this.serverSocket = new ServerSocket(0);
+        startConnectionThread();
     }
 
     public Server(int port) throws IOException {
         this.serverSocket = new ServerSocket(port);
+        startConnectionThread();
     }
 
     public int getPort() {
         return serverSocket.getLocalPort();
+    }
+
+    public Map<UUID, Client> getClients() {
+        return clients;
+    }
+
+    private void startConnectionThread() {
+        new Thread(() -> {
+            while(!serverSocket.isClosed()) {
+                try {
+                    Socket socket = serverSocket.accept();
+                    if(socket == null)
+                        continue;
+                    UUID uuid = UUID.randomUUID();
+                    clients.put(uuid, new Client(uuid, socket));
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }).start();
     }
 }
